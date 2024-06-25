0b923cde25af16314570279320046042a3841c9c
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-06-25 21:22:18 +0300

    Update subscriber-text.php

--- common/mail/subscriber-text.php
+++ common/mail/subscriber-text.php
@@ -1,9 +1,5 @@
 <?php
-/**
- * User: TheCodeholic
- * Date: 4/18/2020
- * Time: 10:27 AM
- */
+
 /** @var $channel \common\models\User */
 /** @var $user \common\models\User */
 ?>
@@ -11,5 +7,4 @@
 Hello <?php echo $channel->username ?>
 User <?php echo $user->username ?> has subscribed to you
 
-FreeCodeTube team
 
